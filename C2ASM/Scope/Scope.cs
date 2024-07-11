006b7068ae3f0cc42240d1999163b98f0d89803a
Vasilis Moulas <vasilismoulas11@gmail.com>
2024-07-11 11:23:33 +0300

    Create Scope Interface

---
+++ C2ASM/Scope/Scope.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2ASM
+{
+    public interface Scope
+    {
+        public String getScopeName(); // do I have a name?
+        public Scope getEnclosingScope(); // am I nested in another?
+        public void define(ASTElement sym); // define sym in this scope
+        public ASTElement resolve(String name); // look up name in scope
+    }
+
+}
